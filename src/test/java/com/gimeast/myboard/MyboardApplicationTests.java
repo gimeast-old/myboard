d970056cf228a2cec9ae32eb2fb7595d71ecd87b
kimdonguk <slasnrndu@gmail.com>
2021-05-13 21:18:38 +0900

    init

---
+++ src/test/java/com/gimeast/myboard/MyboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gimeast.myboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
